577859674919d0b18ea6cdeaa43a452928ba13a0
sharvas <sharvas@gmail.com>
2026-09-02 17:02:58 +0530

    commit

--- src/search/__main__.py
+++ src/search/__main__.py
@@ -8,4 +8,8 @@ from .app import create_app
 
 
 if __name__ == "__main__":
-    uvicorn.run(create_app(), host="127.0.0.1", port=int(os.getenv("SEARCH_PORT", "8000")))
+    uvicorn.run(
+        create_app(),
+        host=os.getenv("HOST", "0.0.0.0"),
+        port=int(os.getenv("PORT", os.getenv("SEARCH_PORT", "8000"))),
+    )
